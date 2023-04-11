e2fc59c71ea21fa7ad136170346a69796aec80c1
amedduman <amedduman@protonmail.com>
2023-04-11 22:48:22 +0300

    Start working on grid

---
+++ Assets/_Game/Scripts/InputManager.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputManager : MonoBehaviour
+{
+    [SerializeField] LayerMask _placementLayerMask;
+    Vector3 _lastRayHittingPos;
+    Camera _cam;
+
+    private void Awake()
+    {
+        _cam = ServiceLocator.Get<Camera>(SerLocID.MainCamera);
+    }
+
+    public Vector3 GetSelectedMapPosition()
+    {
+        Vector3 mousePos = Input.mousePosition;
+        mousePos.z = _cam.nearClipPlane;
+        Ray ray = _cam.ScreenPointToRay(mousePos);
+        RaycastHit hitInfo;
+
+        if(Physics.Raycast(ray, out hitInfo, Mathf.Infinity, _placementLayerMask))
+        {
+            _lastRayHittingPos = hitInfo.point;
+        }
+
+        return _lastRayHittingPos;
+    }
+}
---
+++ Assets/_Game/Scripts/PlacementSystem.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlacementSystem : MonoBehaviour
+{
+    [SerializeField] Transform _selectedGridIndicator;
+    InputManager _inputManager;
+    Grid _grid;
+
+    private void Awake()
+    {
+        _inputManager = ServiceLocator.Get<InputManager>();
+        _grid = ServiceLocator.Get<Grid>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _selectedGridIndicator.position = _inputManager.GetSelectedMapPosition();
+        Vector3Int cellPos = _grid.WorldToCell(_inputManager.GetSelectedMapPosition());
+        _selectedGridIndicator.position = cellPos;
+    }
+}
