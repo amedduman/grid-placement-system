d8bddbfd9f65afbc8ebc38a26b05507655c0d168
amedduman <amedduman@protonmail.com>
2023-04-12 22:33:58 +0300

    Continue

---
+++ Assets/_Game/Scripts/GridObjectDatabaseSO.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu()]
+public class GridObjectDatabaseSO : ScriptableObject
+{
+    public List<GridObject> _gridObjectList;
+}
+
+[System.Serializable]
+public class GridObject
+{
+    [Multiline(10)] public string ShapeInfo;
+    public GameObject ObjectPrefab;
+}
--- Assets/_Game/Scripts/PlacementSystem.cs
+++ Assets/_Game/Scripts/PlacementSystem.cs
@@ -1,10 +1,11 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PlacementSystem : MonoBehaviour
 {
-    [SerializeField] GridPlaceableObject _obj;
+    [SerializeField] GridObjectDatabaseSO _database;
     [SerializeField] Transform _selectedGridIndicator;
     InputManager _inputManager;
     Grid _grid;
@@ -17,10 +18,39 @@ public class PlacementSystem : MonoBehaviour
 
     void Update()
     {
-        _selectedGridIndicator.position = _inputManager.GetSelectedMapPosition();
-        Vector3Int cellPos = _grid.WorldToCell(_inputManager.GetSelectedMapPosition());
-        //_selectedGridIndicator.position = cellPos;
+        var worldPos = _inputManager.GetSelectedMapPosition();
+        Vector3Int cellPos = _grid.WorldToCell(worldPos);
         var cellCenterPos = _grid.GetCellCenterWorld(cellPos);
         _selectedGridIndicator.position = new Vector3(cellCenterPos.x, _selectedGridIndicator.transform.position.y, cellCenterPos.z);
+
+
+        var cellWorldPos = _grid.CellToWorld(cellPos);
+
+
+
+
+        if(Input.GetMouseButtonDown(0))
+        {
+            var objPrf = _database._gridObjectList[0].ObjectPrefab;
+            var obj = Instantiate(objPrf, cellWorldPos, Quaternion.identity);
+            UpdateAvailabityOfCells(cellPos, _database._gridObjectList[0].ShapeInfo);
+        }
+    }
+
+    private void UpdateAvailabityOfCells(Vector3Int startingCellPos, string shapeInfo)
+    {
+        string[] lines = shapeInfo.Split();
+        foreach (var item in lines)
+        {
+            Debug.Log(item);
+        }
+
+        for (int i = 0; i < shapeInfo.Length; i++)
+        {
+            if (shapeInfo[i] == '1')
+            {
+
+            }
+        }
     }
 }
